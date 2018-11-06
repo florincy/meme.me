c6070a39c328a4733cc470bd0c5c9652a17b5127
florincy <simoneflorency@gmail.com>
2018-11-06 12:35:47 -0200

    oi

---
+++ Meme.me/src/main/java/br/edu/iff/meme/me/UsuarioMeme.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.iff.meme.me;
+
+/**
+ *
+ * @author aluno
+ */
+public class UsuarioMeme {
+    String email;
+    String nome;
+    String sobrenome;
+    String nick;
+    String senha;
+    String pais;
+    boolean privatePublic;
+    String bio;
+    
+    
+}
