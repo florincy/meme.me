3c067a1e3b49fe7d105e92fd13e99b61d9692cb0
florincy <simoneflorency@gmail.com>
2018-11-07 01:33:34 -0200

    telas de login e cadastro feitas

--- Meme.me/src/main/java/br/edu/iff/meme/me/UsuarioMeme.java
+++ Meme.me/src/main/java/br/edu/iff/meme/me/UsuarioMeme.java
@@ -10,14 +10,126 @@ package br.edu.iff.meme.me;
  * @author aluno
  */
 public class UsuarioMeme {
-    String email;
-    String nome;
-    String sobrenome;
-    String nick;
-    String senha;
-    String pais;
-    boolean privatePublic;
-    String bio;
+
+    /**
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the sobrenome
+     */
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    /**
+     * @param sobrenome the sobrenome to set
+     */
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    /**
+     * @return the nick
+     */
+    public String getNick() {
+        return nick;
+    }
+
+    /**
+     * @param nick the nick to set
+     */
+    public void setNick(String nick) {
+        this.nick = nick;
+    }
+
+    /**
+     * @return the senha
+     */
+    public String getSenha() {
+        return senha;
+    }
+
+    /**
+     * @param senha the senha to set
+     */
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    /**
+     * @return the pais
+     */
+    public String getPais() {
+        return pais;
+    }
+
+    /**
+     * @param pais the pais to set
+     */
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+    /**
+     * @return the privatePublic
+     */
+    public boolean isPrivatePublic() {
+        return privatePublic;
+    }
+
+    /**
+     * @param privatePublic the privatePublic to set
+     */
+    public void setPrivatePublic(boolean privatePublic) {
+        this.privatePublic = privatePublic;
+    }
+
+    /**
+     * @return the bio
+     */
+    public String getBio() {
+        return bio;
+    }
+
+    /**
+     * @param bio the bio to set
+     */
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+    private String email;
+    private String nome;
+    private String sobrenome;
+    private String nick;
+    private String senha;
+    private String pais;
+    private boolean privatePublic;
+    private String bio;
     
     
 }
