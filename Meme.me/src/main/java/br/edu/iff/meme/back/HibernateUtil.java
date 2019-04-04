289974ef9918eeb60842ca7e9e2c4d8852cd2939
florincy <simoneflorency@gmail.com>
2019-04-04 09:25:51 -0300

    sucesso

--- Meme.me/src/main/java/br/edu/iff/meme/back/HibernateUtil.java
+++ Meme.me/src/main/java/br/edu/iff/meme/back/HibernateUtil.java
@@ -44,7 +44,7 @@ public class HibernateUtil {
         } else {
             System.out.println("Nome: "+user.getNome());
         }
-        /*
+        
         UsuarioMeme usuario = new UsuarioMeme();
         usuario.setBio("Sou eu!");
         usuario.setEmail("eu@g.c");
@@ -60,6 +60,5 @@ public class HibernateUtil {
         tr.commit();
         
         session.close();
-*/
     }
 }
