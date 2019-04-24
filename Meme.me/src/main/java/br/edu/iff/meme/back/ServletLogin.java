cca2395fb2e5595dd4ec249c96bf338e0388cfc6
florincy <simoneflorencygmail.com>
2019-04-24 14:55:30 -0300

    isso aí

---
+++ Meme.me/src/main/java/br/edu/iff/meme/back/ServletLogin.java
@@ -0,0 +1,109 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.iff.meme.back;
+
+import br.edu.iff.meme.me.UsuarioMeme;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import org.hibernate.Session;
+
+/**
+ *
+ * @author aluno
+ */
+public class ServletLogin extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet ServletLogin</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet ServletLogin at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        String email = request.getParameter("email");
+        String senha = request.getParameter("senha");
+        
+        Session session = HibernateUtil.getSession();
+        UsuarioMeme user = (UsuarioMeme) session.createQuery("from UsuarioMeme where email=? and senha=?").setString(0, email).setString(1, senha).uniqueResult();
+        session.close();
+        
+        if (user == null) {
+            response.sendRedirect("erroLogin.html");
+        } else {
+            HttpSession httpSession = request.getSession();
+            //definitivo
+            httpSession.setAttribute("usuarioLogado", user);
+            //provisorio
+            httpSession.setAttribute("nome", user.getNome());
+            response.sendRedirect("principal.jsp");
+        }
+        
+        
+        
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ Meme.me/src/main/java/br/edu/iff/meme/me/UsuarioMeme.java
@@ -0,0 +1,192 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.iff.meme.me;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+
+/**
+ *
+ * @author aluno
+ */
+@Entity
+@Table(name = "user_meme")
+public class UsuarioMeme implements Serializable {
+
+    /**
+     * @return the cdUsuarioMeme
+     */
+    public Integer getCdUsuarioMeme() {
+        return cdUsuarioMeme;
+    }
+
+    /**
+     * @param cdUsuarioMeme the cdUsuarioMeme to set
+     */
+    public void setCdUsuarioMeme(Integer cdUsuarioMeme) {
+        this.cdUsuarioMeme = cdUsuarioMeme;
+    }
+
+    /**
+     * @return the nascimento
+     */
+    public Date getNascimento() {
+        return nascimento;
+    }
+
+    /**
+     * @param nascimento the nascimento to set
+     */
+    public void setNascimento(Date nascimento) {
+        this.nascimento = nascimento;
+    }
+
+    /**
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the sobrenome
+     */
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    /**
+     * @param sobrenome the sobrenome to set
+     */
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    /**
+     * @return the nick
+     */
+    public String getNick() {
+        return nick;
+    }
+
+    /**
+     * @param nick the nick to set
+     */
+    public void setNick(String nick) {
+        this.nick = nick;
+    }
+
+    /**
+     * @return the senha
+     */
+    public String getSenha() {
+        return senha;
+    }
+
+    /**
+     * @param senha the senha to set
+     */
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    /**
+     * @return the pais
+     */
+    public String getPais() {
+        return pais;
+    }
+
+    /**
+     * @param pais the pais to set
+     */
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+    /**
+     * @return the privatePublic
+     */
+    public boolean isPrivado() {
+        return privado;
+    }
+
+    /**
+     * @param privatePublic the privatePublic to set
+     */
+    public void setPrivado(boolean privatePublic) {
+        this.privado = privatePublic;
+    }
+
+    /**
+     * @return the bio
+     */
+    public String getBio() {
+        return bio;
+    }
+
+    /**
+     * @param bio the bio to set
+     */
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+    @Column (name="ds_email")
+    private String email;
+    @Column(name = "nm_first")
+    private String nome;
+    @Column (name="nm_last")
+    private String sobrenome;
+    @Column (name="ds_nick")
+    private String nick;
+    @Column (name="ds_password")
+    private String senha;
+    @Column (name="ds_country")
+    private String pais;
+    @Column(name="tp_private")
+    private boolean privado;
+    @Column(name="ds_bios")
+    private String bio;
+    @Column(name="dt_birth")
+    @Temporal(javax.persistence.TemporalType.DATE)
+    private Date nascimento;
+    @Id
+    @Column(name="cd_user_meme")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sequence_user_meme")
+    @SequenceGenerator(name = "sequence_user_meme", sequenceName = "sq_cd_user_meme")
+    private Integer cdUsuarioMeme;
+    
+    
+}
