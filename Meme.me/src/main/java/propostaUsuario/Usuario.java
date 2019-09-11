c6fb20aa56d35fd5795e9420bd4edfe67917b086
florincy <simoneflorency@gmail.com>
2019-09-11 12:26:59 -0300

    vai

---
+++ Meme.me/src/main/java/propostaUsuario/Usuario.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package propostaUsuario;
+
+/**
+ *
+ * @author aluno
+ */
+public  abstract class Usuario {
+    Int cdUsuario;
+    String nome, sobrenome, nick, email,senha;
+    
+    
+}
---
+++ Meme.me/src/main/java/propostaUsuario/UsuarioMeme.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package propostaUsuario;
+
+/**
+ *
+ * @author aluno
+ */
+public class UsuarioMeme extends Usuario {
+
+    String pais, bio, dataNascimento;
+    Boolean privado;
+
+    public void publica() {
+
+    }
+
+    public void curte() {
+
+    }
+
+    public void comenta() {
+
+    }
+
+    public void compartilha() {
+
+    }
+
+    public void edita() {
+
+    }
+
+}
